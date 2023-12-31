98e3a98c5ffb5e456ee0d78222c0dcc8ea88f46b
TIMER-err <zqq2333@outlook.com>
2023-12-31 17:14:58 +0800

    update

--- src/cn/yapeteam/yolbi/module/impl/HeadUpDisplay.java
+++ src/cn/yapeteam/yolbi/module/impl/HeadUpDisplay.java
@@ -7,8 +7,8 @@ import cn.yapeteam.yolbi.font.cfont.CFontRenderer;
 import cn.yapeteam.yolbi.module.Module;
 import cn.yapeteam.yolbi.module.ModuleCategory;
 import cn.yapeteam.yolbi.module.ModuleInfo;
-import net.minecraft.client.renderer.GlStateManager;
 
+import java.awt.*;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,13 +17,12 @@ import java.util.stream.Collectors;
 public class HeadUpDisplay extends Module {
     @Listener
     private void onRender(EventRender2D e) {
-        GlStateManager.color(1, 1, 1, 1);
         CFontRenderer fontRenderer = YolBi.instance.getFontManager().getJelloRegular18();
-        fontRenderer.drawString(YolBi.name + " " + YolBi.version, 2, 2, -1, true);
+        fontRenderer.drawString(YolBi.name + " " + YolBi.version, 2, 2, new Color(-1).getRGB());
         List<Module> activeModules = YolBi.instance.getModuleManager().getModules().stream().filter(Module::isEnabled).sorted(Comparator.comparingInt(m -> m.getName().length())).collect(Collectors.toList());
         for (int i = 0; i < activeModules.size(); i++) {
             Module module = activeModules.get(i);
-            fontRenderer.drawString(module.getName(), e.getScaledresolution().getScaledWidth() - fontRenderer.getStringWidth(module.getName()) - 2, 2 + i * (fontRenderer.getStringHeight() + 2), -1, true);
+            fontRenderer.drawString(module.getName(), e.getScaledresolution().getScaledWidth() - fontRenderer.getStringWidth(module.getName()) - 2, 2 + i * (fontRenderer.getStringHeight() + 2), new Color(-1).getRGB());
         }
     }
 }
