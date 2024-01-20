84a7c8fa76c1fc97c3bc46b4b1cbce350520df98
TIMER-err <zqq2333@outlook.com>
2024-01-20 19:49:07 +0800

    update

--- src/cn/yapeteam/yolbi/mixin/MixinManager.java
+++ src/cn/yapeteam/yolbi/mixin/MixinManager.java
@@ -8,9 +8,7 @@ import cn.yapeteam.loader.mixin.annotations.Mixin;
 import cn.yapeteam.loader.utils.ClassUtils;
 import cn.yapeteam.yolbi.mixin.injection.*;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Map;
@@ -20,7 +18,7 @@ public class MixinManager {
     public static Transformer transformer;
 
     public static void init() throws Throwable {
-        transformer = new Transformer(MixinManager::getClassBytes);
+        transformer = new Transformer(ClassUtils::getClassBytes);
         add(MixinMinecraft.class);
         add(MixinGuiIngame.class);
         add(MixinEntityPlayerSP.class);
@@ -28,27 +26,6 @@ public class MixinManager {
         add(MixinPlayerControllerMP.class);
         add(MixinBlock.class);
         add(MixinEntityLivingBase.class);
-    }
-
-    public static byte[] getClassBytes(String name) throws Throwable {
-        InputStream classStream = MixinManager.class.getResourceAsStream("/" + name.replace('.', '/') + ".class");
-        if (classStream == null) {
-            byte[] bytes = ClassUtils.getClassBytes(name);
-            if (bytes != null)
-                return bytes;
-            throw new ClassNotFoundException(name);
-        } else {
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            byte[] buf = new byte[1024];
-
-            int len;
-            while ((len = classStream.read(buf)) != -1) {
-                os.write(buf, 0, len);
-            }
-
-            classStream.close();
-            return os.toByteArray();
-        }
     }
 
     private static final File dir = new File("generatedClasses");
