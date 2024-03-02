f75db3679a3cbfe1b954a217b6d956909ba744cd
TIMER-err <zqq2333@outlook.com>
2024-03-02 18:27:08 +0800

    update

--- src/cn/yapeteam/yolbi/module/impl/script/Attacktest.java
+++ src/cn/yapeteam/yolbi/module/impl/script/Attacktest.java
@@ -5,14 +5,13 @@ import cn.yapeteam.yolbi.event.impl.player.EventAttack;
 import cn.yapeteam.yolbi.module.Module;
 import cn.yapeteam.yolbi.module.ModuleCategory;
 import cn.yapeteam.yolbi.module.ModuleInfo;
-import org.lwjgl.input.Keyboard;
 
 /**
  * @author yuxiangll
  * @since 2024/1/7 21:06
  * IntelliJ IDEA
  */
-@ModuleInfo(name = "Attacktest", category = ModuleCategory.SCRIPT, key = Keyboard.KEY_Z)
+@ModuleInfo(name = "Attacktest", category = ModuleCategory.SCRIPT)
 public class Attacktest extends Module {
     @Listener
     public void attack(EventAttack e) {
