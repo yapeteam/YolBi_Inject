ca0a629b962c3bd90c1f57f1983e7af9466be886
TIMER_err <zqq2333@outlook.com>
2024-04-13 22:40:37 +0800

    updated

--- injector/dllmain.c
+++ injector/dllmain.c
@@ -1,30 +1,28 @@
 #include <windows.h>
 #include "jni.h"
 #include "jvmti.h"
+#include <stdlib.h>
 #include <stdio.h>
-
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "UnusedParameter"
-#pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
+#include <dirent.h>
+#include <string.h>
 
 void Inject(JNIEnv *, jvmtiEnv *, const char[260]);
 
-DWORD WINAPI Main(LPVOID parm) {
+DWORD WINAPI Main(LPVOID parm)
+{
     JavaVM *jvm;
     JNIEnv *jniEnv;
     jvmtiEnv *jvmti;
     HMODULE jvmHandle = GetModuleHandle("jvm.dll");
     if (!jvmHandle)
         return 0;
-    typedef jint(JNICALL *fnJNI_GetCreatedJavaVMs)(JavaVM **, jsize, jsize *);
-    fnJNI_GetCreatedJavaVMs JNI_GetCreatedJavaVMs = (fnJNI_GetCreatedJavaVMs) GetProcAddress(jvmHandle,
-                                                                                             "JNI_GetCreatedJavaVMs");
+    typedef jint(JNICALL * fnJNI_GetCreatedJavaVMs)(JavaVM **, jsize, jsize *);
+    fnJNI_GetCreatedJavaVMs JNI_GetCreatedJavaVMs = (fnJNI_GetCreatedJavaVMs)GetProcAddress(jvmHandle, "JNI_GetCreatedJavaVMs");
     if (!JNI_GetCreatedJavaVMs)
         return 0;
-    if (JNI_GetCreatedJavaVMs(&jvm, 1, NULL) != JNI_OK ||
-        (*jvm)->AttachCurrentThread(jvm, (void **) &jniEnv, NULL) != JNI_OK)
+    if (JNI_GetCreatedJavaVMs(&jvm, 1, NULL) != JNI_OK || (*jvm)->AttachCurrentThread(jvm, (void **)&jniEnv, NULL) != JNI_OK)
         return 0;
-    (*jvm)->GetEnv(jvm, (void **) &jvmti, JVMTI_VERSION);
+    (*jvm)->GetEnv(jvm, (void **)&jvmti, JVMTI_VERSION);
     if (!jvmti)
         return 0;
     char userProfile[MAX_PATH];
@@ -36,10 +34,15 @@ DWORD WINAPI Main(LPVOID parm) {
     return 0;
 }
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
-    if (fdwReason == DLL_PROCESS_ATTACH)
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+    {
         CreateThread(NULL, 4096, &Main, NULL, 0, NULL);
+        break;
+    }
+    }
     return TRUE;
 }
-
-#pragma clang diagnostic pop
